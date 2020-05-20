5fdf8aeeecd53822812e5d96a4e5c7096309e5ed
Viktor <biktor12487a@gmail.com>
2020-05-20 23:55:08 +0300

    init

---
+++ src/containers/Cart/Cart.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import productImage from '../../assets/images/product.jpg';
+import './style.sass';
+
+const Cart = () => (
+  <div className="cart">
+    <div className="cart__container">
+      <div className="cart__header">
+        <h2 className="cart__title">Cart</h2>
+        <button type="button" className="cart__button-close">Close</button>
+      </div>
+      <div className="cart__body">
+        {
+                [1, 2, 3].map((id) => (
+                  <div className="cart__item" key={id}>
+                    <div className="cart__item-image-wrap">
+                      <img className="cart__item-image" src={productImage} alt="" />
+                    </div>
+                    <h3 className="cart__item-title">Lorem ipsum dolor.</h3>
+                    <div className="cart__count">
+                      <input className="cart__count-input" value="0" />
+                    </div>
+                    <button type="button" className="cart__item-delete">Delete</button>
+                  </div>
+                ))
+            }
+      </div>
+      <div className="cart__footer">
+        <div className="cart__total">
+          <div className="cart__total-title">
+            Total:
+          </div>
+          <b>1700</b>
+          UAH
+        </div>
+        <button type="button" className="cart__buy button">Order</button>
+      </div>
+    </div>
+  </div>
+);
+
+
+export default Cart;
